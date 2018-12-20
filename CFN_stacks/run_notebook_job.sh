5d9ae513c86d7bf6f4a7bb964853eed9514342d0
tomcatling <tomcatling@gmail.com>
2018-12-20 23:00:35 +0000

    .

--- CFN_stacks/run_notebook_job.sh
+++ CFN_stacks/run_notebook_job.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 IPYNB_FILE=${1}
 IMAGE_TAG=${2}
 
-NB_PATH=${IPYNB_FILE} IMAGE_TAG=${IMAGE_TAG} docker-compose -f ../docker/docker-compose-headless-job.yml up --exit-code-from jupyterlab --no-color
+NB_PATH=${IPYNB_FILE} IMAGE_TAG=${IMAGE_TAG} docker-compose -f ../docker-compose.yml -f headless-compose-override.yml up --exit-code-from jupyterlab --no-color
