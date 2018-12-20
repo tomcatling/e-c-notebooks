215223b8f11985dbe03c58649fd5fb6aa7fc52af
tomcatling <tomcatling@gmail.com>
2018-12-20 22:32:12 +0000

    .

--- CFN_stacks/run_notebook_job.sh
+++ CFN_stacks/run_notebook_job.sh
@@ -3,28 +3,5 @@ set -eo pipefail
 
 IPYNB_FILE=${1}
 
-usage() {
-	cat <<-EOF
 
-	Usage: $0 <path to notebook>
-	  e.g.
-	  $0 notebooks/examples/loading_data_from_datalake.ipynb
-	EOF
-	return
-}
-
-fail_exit() {
-  echo ${1}
-  usage
-  exit 1
-}
-
-if [ -z ${IPYNB_FILE} ]; then
-	fail_exit "Notebooks file not specified"
-fi
-
-if [ ! -f ${IPYNB_FILE} ]; then
-    fail_exit "File: ${IPYNB_FILE} - does not exist"
-fi
-
-SCRIPT_PATH=${IPYNB_FILE} docker-compose -f ../docker/docker-compose-headless-job.yml up --exit-code-from jupyter --no-color
+NB_PATH=${IPYNB_FILE} docker-compose -f ../docker/docker-compose-headless-job.yml up --exit-code-from jupyter --no-color
