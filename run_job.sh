02d3afbcfeb3634b627b5a260491aa5455a52b07
tomcatling <tomcatling@gmail.com>
2018-12-20 22:03:00 +0000

    .

---
+++ run_job.sh
@@ -0,0 +1,30 @@
+#!/bin/bash#!/usr/bin/env bash
+set -eo pipefail
+
+IPYNB_FILE=${1}
+
+usage() {
+	cat <<-EOF
+
+	Usage: $0 <path to notebook>
+	  e.g.
+	  $0 notebooks/examples/loading_data_from_datalake.ipynb
+	EOF
+	return
+}
+
+fail_exit() {
+  echo ${1}
+  usage
+  exit 1
+}
+
+if [ -z ${IPYNB_FILE} ]; then
+	fail_exit "Notebooks file not specified"
+fi
+
+if [ ! -f ${IPYNB_FILE} ]; then
+    fail_exit "File: ${IPYNB_FILE} - does not exist"
+fi
+
+aws cloudformation create-stack --stack-name builder --template-body file://CFN_stacks/job-stack.yaml --parameters file://CFN_stacks/stack-config.json
