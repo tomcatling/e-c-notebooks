742ccc403be7020ace1047aed201306dd5afb9a7
tomcatling <tomcatling@gmail.com>
2018-12-20 20:58:25 +0000

    .

--- run_build.sh
+++ run_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws cloudformation create-stack --stack-name builder --template-body file://CFN_stacks/build_stack.yaml --parameters file://CFN_stacks/stack_config.json
+aws cloudformation create-stack --stack-name builder --template-body file://CFN_stacks/build-stack.yaml --parameters file://CFN_stacks/stack-config.json
