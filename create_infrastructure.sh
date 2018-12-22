5d41201fe0c591448ac1250a546946001264a1db
tomcatling <tomcatling@gmail.com>
2018-12-22 22:13:42 +0000

    .

--- create_infrastructure.sh
+++ create_infrastructure.sh
@@ -2,10 +2,10 @@
 
 echo "Creating infrastructure stack..."
 
-aws cloudformation create-stack --stack-name infrastructure --template-body file://CFN_stacks/infrastructure-stack.yaml --capabilities CAPABILITY_IAM
+#aws cloudformation create-stack --stack-name infrastructure2 --template-body file://CFN_stacks/infrastructure-stack.yaml --capabilities CAPABILITY_IAM
 
 echo "Waiting for stack creation to finish. This will take a few minutes..."
-aws cloudformation wait stack-create-complete --stack-name infrastructure
+#aws cloudformation wait stack-create-complete --stack-name infrastructure2
 
 git_addr=$(cat .git/config | grep url | awk -F '=' '{print $2}' | head -n 1)
 
@@ -15,6 +15,17 @@ exports=json.load(sys.stdin)['Exports'];\
 export_str=str([i['Value'] for i in exports if i.get('Name')=='CodeCommitAddress']);\
 print(export_str.replace('\'','').lstrip('[').rstrip(']'))")
 
+image_tag=$(echo $stack_outputs | python -c "import sys, json; \
+exports=json.load(sys.stdin)['Exports'];\
+export_str=str([i['Value'] for i in exports if i.get('Name')=='ImageTagExport']);\
+print(export_str.replace('\'','').lstrip('[').rstrip(']'))")
+
+image_tag_ESC=$(sed 's/[\*\.\/]/\\&/g' <<<"$image_tag")
+
+echo "$image_tag_ESC"
+
+sed "s/image: none/image: $image_tag_ESC/" docker-compose.yaml > docker-compose.yaml.tmp
+mv docker-compose.yaml.tmp docker-compose.yaml
 
 echo "Adding a remote called codecommit..."
 echo "git remote add codecommit ${cc_addr}"
