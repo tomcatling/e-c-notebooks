f7b3bdc75f20b2426ea5c0332ebb9781eaa63759
tomcatling <tomcatling@gmail.com>
2019-01-14 13:15:09 +0000

    Update jupyterlab_remote.sh

--- jupyterlab_remote.sh
+++ jupyterlab_remote.sh
@@ -7,6 +7,6 @@ echo "Waiting for stack creation to finish..."
 aws cloudformation wait stack-create-complete --stack-name jupyterlab
 
 public_ip=$(aws cloudformation describe-stacks --stack-name jupyterlab --query \
-"Stacks[0].Outputs[?ExportName=='ServerPublicIp'].OutputValue" --output text)
+"Stacks[0].Outputs[?ExportName=='ECNotebooks::ServerPublicIp'].OutputValue" --output text)
 
 echo "...server is running at: ${public_ip}:8888"
