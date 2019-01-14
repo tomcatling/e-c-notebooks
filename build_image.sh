c87ddd1429df53a743e87ba2bcfc205368cf306e
tomcatling <tomcatling@gmail.com>
2019-01-14 13:13:47 +0000

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -8,6 +8,6 @@ echo "Waiting for stack creation to finish..."
 aws cloudformation wait stack-create-complete --stack-name builder
 
 public_ip=$(aws cloudformation describe-stacks --stack-name builder --query \
-"Stacks[0].Outputs[?ExportName=='BuildPublicIp'].OutputValue" --output text)
+"Stacks[0].Outputs[?ExportName=='ECNotebooks::BuildPublicIp'].OutputValue" --output text)
 
 echo "...builder is running at: ${public_ip}"
